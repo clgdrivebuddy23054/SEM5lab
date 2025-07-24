11b97fd28222da5513593fa082f4dfa9d1898ad4
clgdrivebuddy23054 <clgdrivebuddy23054@gmail.com>
2025-07-24 12:33:55 +0530

    Delete dip/1.read_img directory

--- dip/1.read_img/readimg.py
+++
@@ -1,9 +0,0 @@
-import cv2 
-image = cv2.imread(r"C:/23054-AI-008/sem5/dip/image.jpg")
-
-if image is None:
-    print("Error : Image not found.")
-else:
-    cv2.imshow('Displayed Image', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
