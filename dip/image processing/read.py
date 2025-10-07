3e6c7c615f76e8e163091cf8a665e78dde864d6b
clgdrivebuddy23054 <clgdrivebuddy23054@gmail.com>
2025-10-07 11:31:33 +0530

    Delete dip/image processing directory

--- dip/image processing/read.py
+++
@@ -1,9 +0,0 @@
-import cv2 
-image = cv2.imread(r"C:\Users\gioes\Downloads\WhatsApp Image 2025-06-26 at 11.18.47.jpeg")
-
-if image is None:
-    print("Error : Image not found.")
-else:
-    cv2.imshow('Displayed Image', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
